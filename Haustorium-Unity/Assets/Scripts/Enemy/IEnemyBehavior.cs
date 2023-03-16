9a688d54653772a9a5707217074bc7e254099531
mkerndev <96443128+paradoxReborn@users.noreply.github.com>
2023-03-16 11:04:24 -0500

    Removed extraneous namespace, fixed Unity stupid

--- Haustorium-Unity/Assets/Scripts/Enemy/IEnemyBehavior.cs
+++ Haustorium-Unity/Assets/Scripts/Enemy/IEnemyBehavior.cs
@@ -1,17 +1,16 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-namespace Assets.Scripts.Enemy
+
+
+/// <summary>
+/// Call Idle, Attack, or Stun once to begin doing that action.
+/// EnemyBehavior should continue that action until changed again.
+/// </summary>
+public interface IEnemyBehavior
 {
-    /// <summary>
-    /// Call Idle, Attack, or Stun once to begin doing that action.
-    /// EnemyBehavior should continue that action until changed again.
-    /// </summary>
-    public interface IEnemyBehavior
-    {
-        [SerializeField] float stunDuration { get; set; }
-        void Idle();
-        void Attack(GameObject target);
-        void Stun();
-    }
+    [SerializeField] float stunDuration { get; set; }
+    void Idle();
+    void Attack(GameObject target);
+    void Stun();
 }
