e81c87704db88ffe3646d85f56938b772a09e360
mkerndev <96443128+paradoxReborn@users.noreply.github.com>
2023-03-23 10:40:03 -0500

    ShooterPolypBehavior

---
+++ Haustorium-Unity/Assets/ShooterPolypBehavior.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ShooterPolypBehavior : MonoBehaviour, IEnemyBehavior
+{
+    // Objects
+    [SerializeField] GameObject[] vines;
+    Animator[] vineAnims;
+
+    // Settings
+    [SerializeField] float SecBetweenShots = 1.0f;
+    [SerializeField] float InaccuracyDegrees = 5f;
+    [SerializeField] float StunnedTimeSec = 5f;
+    float IEnemyBehavior.stunDuration { get => StunnedTimeSec; set => StunnedTimeSec = value; }
+
+    // Memebers
+    GameObject _target;
+    float _secondsTilShoot;
+
+    // Get all animations
+    void Start()
+    {
+        _secondsTilShoot = SecBetweenShots;
+
+        foreach (GameObject vine in vines)
+        {
+            Animator anim = vine.GetComponent<Animator>();
+            if (anim != null)
+            {
+                vineAnims.Append(anim);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_secondsTilShoot > 0)
+            _secondsTilShoot -= Time.deltaTime;
+        if (_target != null && _secondsTilShoot <= 0)
+            Shoot(_target);
+    }
+
+    /// <summary>
+    /// Spawn a spread of projectiles moving toward target in a cone determined by InaccuracyDegrees
+    /// </summary>
+    /// <param name="target"></param>
+    void Shoot(GameObject target)
+    {
+        // TODO
+    }
+
+    /// <summary>
+    /// Called once when the polyp should begin attacking.
+    /// </summary>
+    /// <param name="target"></param>
+    /// <exception cref="System.NotImplementedException"></exception>
+    void IEnemyBehavior.Attack(GameObject target)
+    {
+        _target = target;
+        _secondsTilShoot = SecBetweenShots;
+        print("Attack animation");
+    }
+
+    void IEnemyBehavior.Die()
+    {
+        _target = null;
+        print("Death animation");
+    }
+
+    void IEnemyBehavior.Idle()
+    {
+        _target = null;
+        print("Idle animation");
+    }
+
+    void IEnemyBehavior.Stun()
+    {
+        _target = null;
+        print("Stun animation.");
+    }
+}
