c6a404a37eccda1134e24351bbbf4157b8c9f1dd
mkerndev <96443128+paradoxReborn@users.noreply.github.com>
2023-03-31 17:19:57 -0500

    Temporary HP display

---
+++ Haustorium-Unity/Assets/HealthDisplay.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HealthDisplay : MonoBehaviour
+{
+    [SerializeField] PlayerHealth health;
+    [SerializeField] TMP_Text text;
+
+    private float m_health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_health != health.health)
+        {
+            m_health = health.health;
+            text.text = "HP: " + m_health;
+        }
+    }
+}
