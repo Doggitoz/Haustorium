c929c2b5552d791488def13b632a336e4f059821
mkerndev <96443128+paradoxReborn@users.noreply.github.com>
2023-04-01 16:38:28 -0500

    Mantrap vine "reach"

---
+++ Haustorium-Unity/Assets/AggroVine.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class AggroVine : MonoBehaviour
+{
+    [SerializeField] EnemyAI controllingAI;
+    Quaternion defaultRotation;
+    Vector3 defaultScale;
+    bool aggro = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (controllingAI == null)
+        {
+            print("vine missing reference to a controlling EnemyAI: " + gameObject);
+            Destroy(this);
+            return;
+        }
+        defaultRotation = gameObject.transform.rotation;
+        defaultScale = gameObject.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aggro)
+            gameObject.transform.LookAt(controllingAI.Target.transform);
+
+        if (!aggro && controllingAI.State == EnemyAI.EnemyState.Aggro)
+        {
+            gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, gameObject.transform.localScale.y, 2 * gameObject.transform.localScale.z);
+            aggro = true;
+        }
+        else if (aggro && controllingAI.State != EnemyAI.EnemyState.Aggro)
+        {
+            gameObject.transform.rotation = defaultRotation;
+            gameObject.transform.localScale = defaultScale;
+            aggro = false;
+        }
+    }
+}
